703159f8cb20af883f508b0e5c9150c88ad4c9b5
pyim <pyim@arinfo.fr>
2019-02-19 09:05:02 +0100

    update structure header

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,27 +1,14 @@
+$(document).ready(function () {
+    
 
-var slideIndex = 1;
-showDivs(slideIndex);
+    
+/////////////////////////////////////////////////////////
+    // Scroll ////////////////////
 
-function plusDivs(n) {
-  showDivs(slideIndex += n);
-}
+    $(".js-scroll-catalogue").click(function () {
+        $("html, body").animate({
+            scrollTop: $(".js-section-catalogue").offset().top
+        }, 1000);
+    });
 
-function currentDiv(n) {
-  showDivs(slideIndex = n);
-}
-
-function showDivs(n) {
-  var i;
-  var x = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("demo");
-  if (n > x.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = x.length}
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";  
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" w3-white", "");
-  }
-  x[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " w3-white";
-}
+});
